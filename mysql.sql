1729edc7be5cd76c720592a52681d1c08b759b6a
PatrykS12 <sp53782@zut.edu.pl>
2025-01-07 11:29:25 +0100

    Add files via upload

--- mysql.sql
+++ mysql.sql
@@ -1,17 +1,21 @@
 -- -- Tworzenie bazy danych
 
 -- Tabela zajecia
-CREATE TABLE zajecia (
-    id_zajec INT AUTO_INCREMENT PRIMARY KEY,
-    id_przedmiotu INT NOT NULL,
-    id_prowadzacego INT NOT NULL,
-    id_sali INT NOT NULL,
-    id_grupy INT NOT NULL,
-    data_zajec DATE NOT NULL,
-    godzina_rozpoczecia TIME NOT NULL,
-    godzina_zakonczenia TIME NOT NULL,
-    FOREIGN KEY (id_przedmiotu) REFERENCES przedmiot(id_przedmiotu) ON DELETE CASCADE,
-    FOREIGN KEY (id_prowadzacego) REFERENCES prowadzacy(id_prowadzacego) ON DELETE CASCADE,
-    FOREIGN KEY (id_sali) REFERENCES sala(id_sali) ON DELETE CASCADE,
-    FOREIGN KEY (id_grupy) REFERENCES grupa(id_grupy) ON DELETE CASCADE
-);
+-- CREATE TABLE zajecia (
+--     id_zajec INT AUTO_INCREMENT PRIMARY KEY,
+--     id_przedmiotu INT NOT NULL,
+--     id_prowadzacego INT NOT NULL,
+--     id_sali INT NOT NULL,
+--     id_grupy INT NOT NULL,
+--     data_zajec DATE NOT NULL,
+--     godzina_rozpoczecia TIME NOT NULL,
+--     godzina_zakonczenia TIME NOT NULL,
+--     FOREIGN KEY (id_przedmiotu) REFERENCES przedmiot(id_przedmiotu) ON DELETE CASCADE,
+--     FOREIGN KEY (id_prowadzacego) REFERENCES prowadzacy(id_prowadzacego) ON DELETE CASCADE,
+--     FOREIGN KEY (id_sali) REFERENCES sala(id_sali) ON DELETE CASCADE,
+--     FOREIGN KEY (id_grupy) REFERENCES grupa(id_grupy) ON DELETE CASCADE
+-- );
+
+ALTER TABLE subject
+RENAME COLUMN typ_przedmiotu TO 'subject_type';
+
